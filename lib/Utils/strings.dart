08d2f3c95fae647e2ed11d33d2d034e8675973fa
Rosemary91 <rosebel.duah47@gmail.com>
2022-02-21 06:18:47 -0800

    update on phone number and otp

--- lib/Utils/strings.dart
+++ lib/Utils/strings.dart
@@ -1,4 +1,17 @@
+import 'package:flutter/painting.dart';
+
 class StringData {
   static const applogo = "assets/nl.png";
   static const appSlogan = "Your health our priority ";
+  static const phoneAuthimg = "assets/pa.jpg";
+  static const otpimg = "assets/OTP.jpg";
+  static const loginText = "Login with Phone Number";
+  static const otpText = "Verification";
+  static const otpcodeText = "We sent you an SMS code on this number";
+
+  static const Color appThemeColor1 = Color.fromRGBO(35, 117, 103, 1);
+  static const Color appThemeColor2 = Color.fromRGBO(163, 153, 144, 1);
+  // static const btnColor =  Color(0x3C8B57);
+
+  // static const Color boxColor = Color.fromRGBO(r, g, b, opacity)
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:netlyfe/Utils/customtheme.dart';
+import 'package:netlyfe/views/login_view.dart';
 import 'package:netlyfe/views/splash_view.dart';
 
 void main() {
@@ -7,15 +9,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SplashView(),
+      theme: CustomTheme().lightTheme,
+      home: const SplashView()
     );
   }
 }
+
