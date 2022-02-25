ce4562345c4982956c1607c2478ee1073248b04d
Rosemary91 <rosebel.duah47@gmail.com>
2022-02-25 10:23:33 -0800

    update on consult doctor page

--- lib/fragments/consult_doctor_fragment.dart
+++ lib/fragments/consult_doctor_fragment.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ConsultDoctorFragment extends StatefulWidget {
@@ -12,10 +13,24 @@ class _ConsultDoctorFragmentState extends State<ConsultDoctorFragment> {
     return Scaffold(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(70),
+          preferredSize: const Size.fromHeight(80),
           child: AppBar(
-            automaticallyImplyLeading: false,
-          ),
+              automaticallyImplyLeading: false,
+              actions: [
+                IconButton(
+                    onPressed: () {}, icon: const Icon(CupertinoIcons.bell))
+              ],
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(50),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text("Welcome, Sophi"),
+                      Text("Tallk to Doctor"),
+                    ],
+                  ),
+                ),
+              )),
         ),
         body: Center(child: Text("Consult Doctor Section")));
   }
